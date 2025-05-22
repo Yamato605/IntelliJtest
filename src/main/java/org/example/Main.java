d3624cc69676d74bcd3ccec87a9eaa236de1dec6
Yamato605 <ymt-956@outlook.jp>
2025-05-22 13:41:40 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("こんにちは");
+    String name = "pull";
+    System.out.println(name + " さん、こんにちは！");
+  }
+}
